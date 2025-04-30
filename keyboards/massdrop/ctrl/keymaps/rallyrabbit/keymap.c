f21bab762640270d55a6ff26c162a83236de4ac9
Adam Oldham <oldhamca@gmail.com>
2025-04-30 19:37:32 -0400

    4/30
    Added Matrix effect with only green (modified digital rain)
    Cleaned up comments

--- keyboards/massdrop/ctrl/keymaps/rallyrabbit/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/rallyrabbit/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,           KC_DEL,  KC_END,  KC_PGDN,
         KC_CAPS,  KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,  KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                                      KC_UP,
-        KC_LCTL,  KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, SC_FUNC, KC_APP,  KC_RCTL,                    KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL,  KC_LGUI, KC_LALT,                      KC_SPC,                             KC_RALT, SC_FUNC, KC_APP,  KC_RCTL,                    KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_MACOS] = LAYOUT(
         KC_ESC,   KC_F1,   KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                     KC_PSCR, KC_SCRL, KC_PAUS,
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,           KC_DEL,  KC_END,  KC_PGDN,
         KC_CAPS,  KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,  KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                                      KC_UP,
-        KC_LCTL,  KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, SC_FUNC, KC_LALT, KC_RCTL,                    KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL,  KC_LALT, KC_LGUI,                      KC_SPC,                             KC_RGUI, SC_FUNC, KC_LALT, KC_RCTL,                    KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FUNCTION] = LAYOUT(
         KC_SLEEP, MD_BOOT, _______,    EE_CLR,  _______, _______, _______, _______, _______, KC_MPLY,KC_MSTP, KC_MPRV, KC_MNXT,                     KC_MUTE, _______, _______,
@@ -832,11 +832,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         case KC_MAC:
             bIsWindowsKeyboard = false;
             tap_code16(DF(_MACOS));
+            set_single_persistent_default_layer(_MACOS);
             return false;
 
         case KC_WIN:
             bIsWindowsKeyboard = true;
             tap_code16(DF(_WINDOWS));
+            set_single_persistent_default_layer(_WINDOWS);
             return false;
 
         case KC_SLEEP:
